0c6badda48de25c856bc3c308228e46cfb44bb80
Valentinonai <137069763+Valentinonai@users.noreply.github.com>
2023-11-13 13:59:31 +0100

    Initial commit

---
+++ src/main/java/Esercizio13112023/Esercizio13112023/Esercizio13112023Application.java
@@ -0,0 +1,13 @@
+package Esercizio13112023.Esercizio13112023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Esercizio13112023Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Esercizio13112023Application.class, args);
+	}
+
+}
